06e607fee71fac23c827e9c94c3aef8cce8d2b91
arielperalta144 <arielperalta144@gmail.com>
2022-10-19 17:57:38 -0300

    Tablas y Script de base de datos

---
+++ Base_datos_script.sql
@@ -0,0 +1,397 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: carta_online_ispc
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoria` (
+  `idcategoria` int NOT NULL,
+  `delibery` varchar(45) DEFAULT NULL,
+  `salon` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idcategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `idClientes` int NOT NULL,
+  `nombre_cliente` varchar(25) DEFAULT NULL,
+  `telefono` int DEFAULT NULL,
+  `direccion` varchar(20) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idClientes`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comentario`
+--
+
+DROP TABLE IF EXISTS `comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comentario` (
+  `idcomentario` int NOT NULL,
+  `asunto` varchar(45) DEFAULT NULL,
+  `comentario` varchar(45) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  PRIMARY KEY (`idcomentario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentario`
+--
+
+LOCK TABLES `comentario` WRITE;
+/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `consumicion`
+--
+
+DROP TABLE IF EXISTS `consumicion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `consumicion` (
+  `idConsumicion` int NOT NULL,
+  `nombre_bebida` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  `tamaño` varchar(45) DEFAULT NULL,
+  `comentario` varchar(45) DEFAULT NULL,
+  `precio` int DEFAULT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  `imagen` varchar(45) DEFAULT NULL,
+  `iditem_consumicion` int DEFAULT NULL,
+  PRIMARY KEY (`idConsumicion`),
+  KEY `idconsumicion_idx` (`iditem_consumicion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `consumicion`
+--
+
+LOCK TABLES `consumicion` WRITE;
+/*!40000 ALTER TABLE `consumicion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `consumicion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleado`
+--
+
+DROP TABLE IF EXISTS `empleado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleado` (
+  `idempleado` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `foto` varchar(45) DEFAULT NULL,
+  `mesa_idmesa` int DEFAULT NULL,
+  PRIMARY KEY (`idempleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleado`
+--
+
+LOCK TABLES `empleado` WRITE;
+/*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factura`
+--
+
+DROP TABLE IF EXISTS `factura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `factura` (
+  `idfactura` int NOT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `descuento` decimal(10,0) DEFAULT NULL,
+  `forma_pago` int DEFAULT NULL,
+  `iva` decimal(10,0) DEFAULT NULL,
+  `precio_total` int DEFAULT NULL,
+  `idpedido` int DEFAULT NULL,
+  `idcliente` int DEFAULT NULL,
+  PRIMARY KEY (`idfactura`),
+  KEY `idcliente_idx` (`idcliente`),
+  KEY `idpedido_idx` (`idpedido`),
+  CONSTRAINT `idcliente` FOREIGN KEY (`idcliente`) REFERENCES `clientes` (`idClientes`),
+  CONSTRAINT `idpedido` FOREIGN KEY (`idpedido`) REFERENCES `pedidos` (`idpedidos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `factura`
+--
+
+LOCK TABLES `factura` WRITE;
+/*!40000 ALTER TABLE `factura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `factura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_consumicion`
+--
+
+DROP TABLE IF EXISTS `item_consumicion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item_consumicion` (
+  `iditem_consumicion` int NOT NULL,
+  `cantidad` int DEFAULT NULL,
+  `detalle_idpedido` int DEFAULT NULL,
+  `idConsumicion` int DEFAULT NULL,
+  PRIMARY KEY (`iditem_consumicion`),
+  KEY `detalle_idpedido_idx` (`detalle_idpedido`),
+  CONSTRAINT `detalle_idpedido` FOREIGN KEY (`detalle_idpedido`) REFERENCES `pedidos` (`idpedidos`),
+  CONSTRAINT `idconsumicion` FOREIGN KEY (`iditem_consumicion`) REFERENCES `consumicion` (`idConsumicion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_consumicion`
+--
+
+LOCK TABLES `item_consumicion` WRITE;
+/*!40000 ALTER TABLE `item_consumicion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_consumicion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_menu`
+--
+
+DROP TABLE IF EXISTS `item_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item_menu` (
+  `iditem_menu` int NOT NULL,
+  `idpedidos` int DEFAULT NULL,
+  `idmenu` int DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  PRIMARY KEY (`iditem_menu`),
+  KEY `idpedidos_idx` (`idpedidos`),
+  KEY `idmenu_idx` (`idmenu`),
+  CONSTRAINT `idmenu` FOREIGN KEY (`idmenu`) REFERENCES `menu` (`idmenu`),
+  CONSTRAINT `idpedidos` FOREIGN KEY (`idpedidos`) REFERENCES `pedidos` (`idpedidos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_menu`
+--
+
+LOCK TABLES `item_menu` WRITE;
+/*!40000 ALTER TABLE `item_menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menu` (
+  `idmenu` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `cantidad_plato` int DEFAULT NULL,
+  `precio` int DEFAULT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  `imagen` varchar(45) DEFAULT NULL,
+  `comentario_plato` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idmenu`),
+  CONSTRAINT `comentario_plato` FOREIGN KEY (`idmenu`) REFERENCES `comentario` (`idcomentario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mesa`
+--
+
+DROP TABLE IF EXISTS `mesa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mesa` (
+  `idmesa` int NOT NULL,
+  `numero` int DEFAULT NULL,
+  `libre` tinyint DEFAULT NULL,
+  `max_personas` int DEFAULT NULL,
+  `num_mesas` int DEFAULT NULL,
+  `idempleado` int DEFAULT NULL,
+  `idclientes` int DEFAULT NULL,
+  PRIMARY KEY (`idmesa`),
+  KEY `idclientes_idx` (`idclientes`),
+  CONSTRAINT `idclientes` FOREIGN KEY (`idmesa`) REFERENCES `clientes` (`idClientes`),
+  CONSTRAINT `idempleado` FOREIGN KEY (`idmesa`) REFERENCES `empleado` (`idempleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mesa`
+--
+
+LOCK TABLES `mesa` WRITE;
+/*!40000 ALTER TABLE `mesa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mesa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedidos`
+--
+
+DROP TABLE IF EXISTS `pedidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedidos` (
+  `idpedidos` int NOT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `precio` int DEFAULT NULL,
+  `pagado` tinyint DEFAULT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  `activo` tinyint DEFAULT NULL,
+  `confirmado` tinyint DEFAULT NULL,
+  `idcategoria` int DEFAULT NULL,
+  PRIMARY KEY (`idpedidos`),
+  CONSTRAINT `idcategoria` FOREIGN KEY (`idpedidos`) REFERENCES `categoria` (`idcategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedidos`
+--
+
+LOCK TABLES `pedidos` WRITE;
+/*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reserva`
+--
+
+DROP TABLE IF EXISTS `reserva`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reserva` (
+  `idReserva` int NOT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `finalizada` datetime DEFAULT NULL,
+  `idmesa` int DEFAULT NULL,
+  PRIMARY KEY (`idReserva`),
+  KEY `idmesa_idx` (`idmesa`),
+  CONSTRAINT `idmesa` FOREIGN KEY (`idmesa`) REFERENCES `mesa` (`idmesa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reserva`
+--
+
+LOCK TABLES `reserva` WRITE;
+/*!40000 ALTER TABLE `reserva` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reserva` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `idusuario` int NOT NULL,
+  `clave` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `res_idreserva` int DEFAULT NULL,
+  `idreserva` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idusuario`),
+  KEY `res_idreserva_idx` (`res_idreserva`),
+  CONSTRAINT `res_idreserva` FOREIGN KEY (`res_idreserva`) REFERENCES `reserva` (`idReserva`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-19 17:42:25
