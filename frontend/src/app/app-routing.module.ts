79a1b2be7f480785083335b8c1a3cfa63aa70337
MatiasPedernera <matias.pedernerax8@gmail.com>
2023-05-07 18:22:00 -0300

    Feature: Seccion Home y Carta

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { CartaComponent } from './carta/carta.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
   {path: 'nosotros', component:NosotrosComponent},
   {path: 'contactanos', component:ContactanosComponent},
-  {path: 'perfil', component:PerfilComponent}
+  {path: 'perfil', component:PerfilComponent},
+  {path: 'carta', component:CartaComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,14 +7,20 @@ import { NosotrosComponent } from './nosotros/nosotros.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { CartaComponent } from './carta/carta.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CartaComponent,
     NosotrosComponent,
     FooterComponent,
     ContactanosComponent,
-    PerfilComponent
+    PerfilComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
