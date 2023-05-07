24c264b4903ba8c57e611d59c10e4741f740560d
YampX <juanpablodriz@gmail.com>
2023-05-07 12:32:25 -0300

    Se migró sección perfil

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NosotrosComponent } from './nosotros/nosotros.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 const routes: Routes = [
-  {path: 'nosotros', component:NosotrosComponent}
+  {path: 'nosotros', component:NosotrosComponent},
+  {path: 'perfil', component:PerfilComponent}
 ];
 
 @NgModule({
